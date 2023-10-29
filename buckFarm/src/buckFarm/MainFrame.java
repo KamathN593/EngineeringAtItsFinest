c9f2cc3774645a555d6f0d5c941e0835d9743fb1
Manasi <86559162+ms2023@users.noreply.github.com>
2023-10-28 20:00:07 -0400

    Creates the first page of the app

--- buckFarm/src/buckFarm/MainFrame.java
+++ buckFarm/src/buckFarm/MainFrame.java
@@ -5,6 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JInternalFrame;
 
 public class MainFrame extends JFrame {
 
@@ -37,6 +49,49 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel pageTitlePanel = new JPanel();
+		contentPane.add(pageTitlePanel);
+		pageTitlePanel.setLayout(new CardLayout(0, 0));
+		
+		JLabel pageTitle = new JLabel("Farms to Buckeyes");
+		pageTitle.setFont(new Font("Freestyle Script", Font.PLAIN, 52));
+		pageTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		pageTitlePanel.add(pageTitle, "name_568084855432200");
+		
+		JLabel lblNewLabel = new JLabel("");
+		pageTitlePanel.add(lblNewLabel, "name_573907829540600");
+		lblNewLabel.setLabelFor(this);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\manas\\Downloads\\brutusInCornucopia.png"));
+		
+		JPanel menuPanel = new JPanel();
+		contentPane.add(menuPanel);
+		menuPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setFont(new Font("Freestyle Script", Font.PLAIN, 30));
+		menuPanel.add(menuBar);
+		
+		JMenuItem homeMenuItem = new JMenuItem("Home");
+		homeMenuItem.setForeground(new Color(0, 0, 0));
+		homeMenuItem.setFont(new Font("Freestyle Script", Font.PLAIN, 30));
+		menuBar.add(homeMenuItem);
+		
+		JMenuItem shopperMenuItem = new JMenuItem("Shopper");
+		shopperMenuItem.setFont(new Font("Freestyle Script", Font.PLAIN, 30));
+		menuBar.add(shopperMenuItem);
+		
+		JMenuItem profileMenuItem = new JMenuItem("Profile");
+		profileMenuItem.setFont(new Font("Freestyle Script", Font.PLAIN, 30));
+		menuBar.add(profileMenuItem);
+		
+		JMenuItem farmerMenuItem = new JMenuItem("Farmer");
+		farmerMenuItem.setFont(new Font("Freestyle Script", Font.PLAIN, 30));
+		menuBar.add(farmerMenuItem);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
 	}
 
 }
